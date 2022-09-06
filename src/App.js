da6af80d551729561d84d1a544d60b1cf35a6f31
Biswajit sikdar <biswajitsikdar51@gmail.com>
2022-09-06 19:03:22 +0600

    Tailwind css add and configaretion

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
+import Home from "./Page/Home";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Home />
+      <h1 className="text-3xl font-bold text-sky-500 ">Hello world!</h1>
+    </div>
+  );
 }
 
 export default App;
