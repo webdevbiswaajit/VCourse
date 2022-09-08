4cd137b1563f6ecfc2f42f5718dc5a37ccd00bd5
Biswajit sikdar <biswajitsikdar51@gmail.com>
2022-09-08 15:00:35 +0600

    Learn with design

--- src/Component/HomeElement/LearnWith.js
+++ src/Component/HomeElement/LearnWith.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Icon } from "@iconify/react";
+import learn from "../../Img/learnwith.jpg";
 
 function LearnWith() {
   return (
@@ -9,7 +10,7 @@ function LearnWith() {
           <h2 className=" text-center text-heading font-medium ">
             Why You'll Learn with Us
           </h2>
-          <div className="grid grid-cols-3 mt-16 gap-5">
+          <div className="grid grid-cols-3 mt-16 mb-28 gap-5">
             <div className=" bg-white p-10 flex flex-col items-center ">
               <Icon
                 className=" text-khaki text-5xl  mb-4 "
@@ -59,6 +60,79 @@ function LearnWith() {
                 to learn quickly, and it's even more vital to eliminate any
                 remaining doubts regarding the lesson you're receiving.{" "}
               </p>
+            </div>
+          </div>
+          <div className=" grid grid-cols-2 gap-5">
+            <div className=" ">
+              <h2 className=" text-5xl text-black font-slab font-medium">
+                Vcourse Creative <br /> Learning Steps
+              </h2>
+              <div className="flex mt-8">
+                <Icon
+                  className=" text-khaki text-5xl mr-5"
+                  icon="akar-icons:circle-chevron-right-fill"
+                />
+
+                <div className="">
+                  <h2 className=" font-rly font-medium text-black text-xl mt-2">
+                    Find What Suits You Most.
+                  </h2>
+                  <p className=" text-black  text-lg mt-3 font-rly">
+                    Learn what make sense to you and find best course from
+                    Vcourse. Learn which grab your attention mostly and you're
+                    highly interested.
+                  </p>
+                </div>
+              </div>
+              <div className="flex mt-8">
+                <Icon
+                  className=" text-khaki text-6xl mr-5"
+                  icon="akar-icons:circle-chevron-right-fill"
+                />
+
+                <div className="">
+                  <h2 className=" font-rly font-medium text-black text-xl mt-4">
+                    Get ready and Make Your Own Place.
+                  </h2>
+                  <p className=" text-black  text-lg mt-3">
+                    Be prepared about your class and ask questions to instructor
+                    if you don't understand his/her lessons. Attend every quiz,
+                    classes and try to improve.
+                  </p>
+                </div>
+              </div>
+
+              <div className="flex mt-8">
+                <Icon
+                  className=" text-khaki text-7xl mr-5"
+                  icon="akar-icons:circle-chevron-right-fill"
+                />
+
+                <div className="">
+                  <h2 className=" font-rly font-medium text-black text-xl mt-5">
+                    Practice and Become Expert on Your Field.
+                  </h2>
+                  <p className=" text-black  text-lg mt-3">
+                    Practice more and more to become expert on your field.
+                    Without practicing no one can be successful. Mind one thing
+                    that everyone values on your expertises not on your outward!
+                  </p>
+                </div>
+              </div>
+              <div className=" mt-10 flex">
+                <button className=" bg-khaki px-5 py-4 flex items-center rounded-md">
+                  <Icon
+                    className=" mr-2.5 text-black text-xl"
+                    icon="bi:bookmark-fill"
+                  />
+                  <span className=" text-black text-sm font-medium">
+                    Start Learn Today
+                  </span>
+                </button>
+              </div>
+            </div>
+            <div className="">
+              <img src={learn} alt="Leanr Banner" />
             </div>
           </div>
         </div>
