2e20ca0852bd840ac3e5a9cba6bb5f008bf5c0b9
Biswajit sikdar <biswajitsikdar51@gmail.com>
2022-09-29 16:20:48 +0600

    update menu

--- src/Component/Common/Menu.js
+++ src/Component/Common/Menu.js
@@ -9,25 +9,28 @@ function Menu() {
     <>
       <div className="menu_bar">
         <div className="container mx-auto main_menu ">
-          <div className="logo_area">
-            <img src={logo} alt="....." />
+          <div className="logo_area ml-2 py-3 flex items-center justify-between">
+            <div className=" text-3xl text-black font-bold">
+              <Icon icon="gg:menu-right-alt" />
+            </div>
+            <img className="" src={logo} alt="....." />
           </div>
           <div className="total_menu">
             <div className="">
               <ul className="menu_item">
-                <li className="  text-sm text-black font-slab  p-7 hover:bg-khaki">
+                <li className="  text-sm text-black font-slab px-7  hover:bg-khaki">
                   <Link to="/home">Home</Link>
                 </li>
-                <li className="  text-sm text-black font-slab  p-7 hover:bg-khaki">
+                <li className="  text-sm text-black font-slab  px-7   hover:bg-khaki">
                   <Link to="/courses">Courses</Link>
                 </li>
-                <li className="  text-sm text-black font-slab  p-7 hover:bg-khaki">
+                <li className="  text-sm text-black font-slab  px-7   hover:bg-khaki">
                   <Link to="/about">About</Link>
                 </li>
-                <li className="  text-sm text-black font-slab  p-7 hover:bg-khaki">
+                <li className="  text-sm text-black font-slab  px-7   hover:bg-khaki">
                   <Link to="/blog">Blog</Link>
                 </li>
-                <li className="  text-sm text-black font-slab  p-7 hover:bg-khaki">
+                <li className="  text-sm text-black font-slab  px-7   hover:bg-khaki">
                   <Link to="/contact">Contact</Link>
                 </li>
                 <li className="  ml-4 text-sm text-black font-slab bg-khaki px-4 py-2.5 rounded-md text-center">
@@ -37,7 +40,7 @@ function Menu() {
                 </li>
               </ul>
             </div>
-            <div className="nav_icon">
+            <div className="nav_icon p-4">
               <Link
                 className=" bg-khaki p-2.5 rounded-full text-white font-bold mr-4"
                 to="/sign-in"
